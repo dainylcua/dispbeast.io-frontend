cf4498ead613b6e8b87fb8a9de7d41f7ce7c8ad8
Dainyl Cua <dainylcua@gmail.com>
2021-10-30 21:05:20 -0500

    Improved layout on a few pages

--- src/pages/Character/Character.js
+++ src/pages/Character/Character.js
@@ -2,11 +2,11 @@ import TextCard from "../../components/TextCard"
 
 const Character = () => {
     return(
-        <div className="flex flex-row mx-auto space-x-8">
+        <div className="flex flex-row h-48 mx-auto space-x-8">
             <div className="w-2/3 border">
                 Big Box
             </div>
-            <div className="w-1/3 space-y-8">
+            <div className="flex flex-col w-1/3 space-y-8">
                 <div className="border">
                     Box1
                 </div>
---
+++ src/pages/Character/Compare.js
@@ -0,0 +1,14 @@
+const Compare = () => {
+    return(
+        <div className="flex flex-row h-48 mx-auto space-x-8">
+            <div className="w-1/2 border">
+                Big Box
+            </div>
+            <div className="w-1/2 border">
+                Big Box
+            </div>
+        </div>
+    )
+}
+
+export default Compare
