1620160d4b95c2e3e6590beca088e51f713f123b
Dainyl Cua <dainylcua@gmail.com>
2021-11-04 21:28:41 -0500

    Fixes navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,25 @@
 import { Link } from 'react-router-dom'
 import { faGoogle } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useState } from 'react'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = (props) => {
-    const [ navToggle, setNavToggle ] = useState(false)
 
     return(
-        <nav className="z-10 flex items-center justify-between h-16 text-2xl font-medium text-white bg-gray-900">
-            <Link to="/" className="pl-8 text-3xl font-semibold text-purple-400 hover:text-purple-300">dispbeast.io</Link>
-            <div className="flex md:hidden" onClick={(prevState) => setNavToggle(!prevState)}>
-                <FontAwesomeIcon className="mr-8 flex-end" icon={faBars} />
-            </div>
+        <nav className="z-10 flex flex-col items-center justify-between h-auto p-6 font-medium text-white bg-gray-900 md:flex-row md:h-16 text-md md:text-2xl">
+            <Link to="/" className="text-3xl font-semibold text-purple-400 hover:text-purple-300">dispbeast.io</Link>
             {
                 props.user ?
                 <>
-                    <div className={`${ navToggle ? null : 'hidden'} md:visible md:flex flex-col md:flex-row gap-6`}>
-                    <Link to="/listings" className=" hover:text-purple-300">listings</Link>
-                    <Link to="/character" className="hover:text-purple-300">character</Link>
-                    <Link to="/adventure" className="hover:text-purple-300">adventure</Link>
-                    </div>
-                    <div className={`${ navToggle ? null : 'hidden' } md:flex md:visible pr-8`}>
+                    <div className={`md:flex flex-col md:flex-row gap-6 space-x-6 last:flex-end`}>
+                        <Link to="/listings" className=" hover:text-purple-300">listings</Link>
+                        <Link to="/character" className="hover:text-purple-300">character</Link>
+                        <Link to="/adventure" className="hover:text-purple-300">adventure</Link>
                         <Link to="/user/dashboard" className="hover:text-purple-300">dashboard</Link>
                     </div>
                 </>
                 :
                 <>
-                    <div className={`${ navToggle ? null : 'hidden' } md:flex md:visible flex-col pr-6 gap-6`}>
+                    <div className={`md:flex md:flex-row flex-col gap-6`}>
                         <div className="flex flex-row items-center">
                         <FontAwesomeIcon icon={faGoogle} className="mr-2"/>
                         <Link to="/user/dashboard" className="hover:text-purple-300">login</Link>
