1bfd106cea60667e3e4e5c1a190194b6cf165305
Dainyl Cua <dainylcua@gmail.com>
2021-11-04 21:30:49 -0500

    Loading card nicer

--- src/components/LoadingCard.js
+++ src/components/LoadingCard.js
@@ -1,5 +1,5 @@
 const LoadingCard = () => (
-    <div className="flex-auto mx-auto overflow-hidden bg-gray-900 rounded-lg shadow-md animate-pulse" >
+    <div className="flex-auto mx-auto mt-20 overflow-hidden bg-gray-900 rounded-lg shadow-md animate-pulse" >
         <div className="text-white" >
             <div className="flex items-center justify-center w-full p-4 h-80 flex-grow-2">
                 <div className="font-semibold text-center ">
